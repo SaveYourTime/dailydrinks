bb96ebfcee1b0fc076d670f382fe21d3ddbf3090
Aaron Lu <aaron_lu1@yahoo.com>
2019-12-08 15:44:48 +0800

    Updates

---
+++ precache-manifest.7bff45e7077b5269dd19c3f99f46d475.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf00c6a0352fd1870405b46f405888b",
+    "url": "/dailydrinks/index.html"
+  },
+  {
+    "revision": "ae60dea6ecd20affb022",
+    "url": "/dailydrinks/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "109338aa56d8e1d724fc",
+    "url": "/dailydrinks/static/css/main.15157899.chunk.css"
+  },
+  {
+    "revision": "ae60dea6ecd20affb022",
+    "url": "/dailydrinks/static/js/2.405e7a9e.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/dailydrinks/static/js/2.405e7a9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "109338aa56d8e1d724fc",
+    "url": "/dailydrinks/static/js/main.b00172b4.chunk.js"
+  },
+  {
+    "revision": "dd327f26bf46cd2caa6e",
+    "url": "/dailydrinks/static/js/runtime-main.401560a9.js"
+  }
+]);
